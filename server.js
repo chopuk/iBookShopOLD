93cf4a76aa061c5e22c36b494277e4ddd59ef129
Chris Harris <chopuk@gmail.com>
2022-09-04 15:40:51 +0100

    Updates

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express')
+const expressSession = require('express-session')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
+const fs = require('fs')
+const path = require('path')
+const logger = require('morgan')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const passport = require('passport')
 
 const app = express()
 
 app.get('/', (req, res, err) => {
     res.send('This is my little Bookshop!!!')
+})
+
+app.get('/amit', (req, res, err) => {
+    res.send('I love Amit - the most beautiful girl in the world!!!!')
 })
 
 dotenv.config();
